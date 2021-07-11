6d4695251612932fe62c3c2dde029f32f0e78428
Stanislav <stani.petrosyan@gmail.com>
2021-07-11 16:14:17 +0200

    refactor

--- handlers/login.go
+++ handlers/login.go
@@ -5,18 +5,6 @@ import "github.com/gin-gonic/gin"
 func LoginApi(context *gin.Context) {
 	var form UserForm
 	context.ShouldBind(&form)
-
-	/* 	println(form.Username)
-	   	println(form.Password)
-
-	   	context.Request.ParseMultipartForm(1000)
-	   	println("cu")
-	   	println(context.GetPostForm("username"))
-	   	context.Request.PostForm.Add("username", "admin")
-	   	username := context.PostForm("username")
-	   	password := context.PostForm("password")
-	   	println("username")
-	   	println(context.Request.PostForm.Get("username")) */
 	if form.Username == "admin" && form.Password == "admin" {
 		context.JSON(200, nil)
 	} else {
