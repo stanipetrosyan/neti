231d4d43a3071dde52b18938f6cdf3e0dae041c7
Stanislav <stani.petrosyan@gmail.com>
2021-07-11 15:49:34 +0200

    tested login api

--- handlers/login.go
+++ handlers/login.go
@@ -3,17 +3,28 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func LoginApi(context *gin.Context) {
-	context.Request.ParseMultipartForm(1000)
-	println("cu")
-	println(context.GetPostForm("username"))
-	context.Request.PostForm.Add("username", "admin")
-	username := context.PostForm("username")
-	password := context.PostForm("password")
-	println("username")
-	println(context.Request.PostForm.Get("username"))
-	if username == "admin" && password == "admin" {
+	var form UserForm
+	context.ShouldBind(&form)
+
+	/* 	println(form.Username)
+	   	println(form.Password)
+
+	   	context.Request.ParseMultipartForm(1000)
+	   	println("cu")
+	   	println(context.GetPostForm("username"))
+	   	context.Request.PostForm.Add("username", "admin")
+	   	username := context.PostForm("username")
+	   	password := context.PostForm("password")
+	   	println("username")
+	   	println(context.Request.PostForm.Get("username")) */
+	if form.Username == "admin" && form.Password == "admin" {
 		context.JSON(200, nil)
 	} else {
 		context.JSON(403, nil)
 	}
+}
+
+type UserForm struct {
+	Username string `form:"username" binding:"required"`
+	Password string `form:"password" binding:"required"`
 }
--- handlers/login_test.go
+++ handlers/login_test.go
@@ -2,10 +2,11 @@ package handlers
 
 import (
 	"bytes"
+	"io"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
-	"net/textproto"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -13,30 +14,41 @@ import (
 )
 
 func TestLoginApi(t *testing.T) {
+	router := setupRouter()
 	t.Run("should return 200", func(t *testing.T) {
-		var json = []byte(`{"username": "admin"}`)
-
 		body := &bytes.Buffer{}
 		writer := multipart.NewWriter(body)
-
-		part, _ := writer.CreatePart(textproto.MIMEHeader{"Content-Type": {"application/json"}})
-		part.Write(json)
-
-		/* fw, _ := writer.CreateFormField("username")
+		fw, _ := writer.CreateFormField("username")
 		io.Copy(fw, strings.NewReader("admin"))
 		fw, _ = writer.CreateFormField("password")
-		io.Copy(fw, strings.NewReader("admin")) */
+		io.Copy(fw, strings.NewReader("admin"))
+
 		writer.Close()
 
-		println(body.String())
-
-		request, _ := http.NewRequest("POST", "/login", body)
+		request, _ := http.NewRequest("POST", "/login", bytes.NewReader(body.Bytes()))
 		request.Header.Set("Content-Type", writer.FormDataContentType())
 		response := httptest.NewRecorder()
-		router := setupRouter()
 		router.ServeHTTP(response, request)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("should return 403 if login credentials are wrong", func(t *testing.T) {
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+		fw, _ := writer.CreateFormField("username")
+		io.Copy(fw, strings.NewReader("wrong"))
+		fw, _ = writer.CreateFormField("password")
+		io.Copy(fw, strings.NewReader("wrong"))
+
+		writer.Close()
+
+		request, _ := http.NewRequest("POST", "/login", bytes.NewReader(body.Bytes()))
+		request.Header.Set("Content-Type", writer.FormDataContentType())
+		response := httptest.NewRecorder()
+		router.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusForbidden, response.Code)
 	})
 }
 
