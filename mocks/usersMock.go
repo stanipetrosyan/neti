8613983ff9c547797f66461702114a676d440c09
Stanislav <stani.petrosyan@gmail.com>
2021-08-12 22:27:20 +0200

    added users mock

---
+++ mocks/usersMock.go
@@ -0,0 +1,17 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+type UsersMock struct {
+	mock.Mock
+}
+
+func (m UsersMock) Add(username string, password string) bool {
+	args := m.Called(username, password)
+	return args.Bool(0)
+}
+
+func (m UsersMock) FindBy(username string) (string, string) {
+	args := m.Called(username)
+	return args.String(0), args.String(1)
+}
